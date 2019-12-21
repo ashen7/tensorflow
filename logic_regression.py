220679c1d4272e3d544329ce0f7b09994c6e9015
ashen7 <986300260@qq.com>
2019-12-21 21:56:49 +0800

    delete

--- logic_regression.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-
-def main():
-    mnist = input_data.read_data_sets('data/', one_hot=True)
-
-if __name__ == '__main__':
-    main()
